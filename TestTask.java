7329e929a98a10ae1b11443b4f333f8372779483
sqdselo <ivantangatarov@gmail.com>
2024-06-14 15:35:37 +0400

    Добавил классы, с которыми нужно будет работать

--- TestTask.java
+++ TestTask.java
@@ -9,9 +9,26 @@ public class TestTask {
         System.out.println("#write, #read, #statistics");
         System.out.println("Важно! следите за правильностью написания команды");
         while(flag){
+            System.out.println("------------------------------------------------------------------------------");
             System.out.println("Введите команду: ");
             String currentCommand = scan.nextLine();
             if (Arrays.asList(array).contains(currentCommand)){
+                switch(currentCommand){
+                    case "#read":
+                        Read read = new Read();
+                        System.out.println("Считывание из файла");
+                        break;
+
+                    case "#write":
+                        Write write = new Write();
+                        System.out.println("Запись в файл");
+                        break;
+
+                    case "#statistics":
+                        Statistics statistics = new Statistics();
+                        System.out.println("Статистика ");
+                        break;
+                }
             }
             else{
                 System.out.println("Неверная команда. Попробуйте еще раз");
